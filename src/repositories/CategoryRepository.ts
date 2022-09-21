349044b6bbc2b28ff01fc0d404121e5403dcb48a
flpdorea <felipeldorea@gmail.com>
2022-09-21 20:47:16 -0300

    refactoring for liskov substitution principle

--- src/repositories/CategoryRepository.ts
+++ src/repositories/CategoryRepository.ts
@@ -1,18 +1,16 @@
 import { Category } from "../model/Category"
+import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository"
 
-interface ICreateCategoryDTIO {
-    name: string,
-    description: string
-}
 
-class CategoriesRepository {
+
+class CategoriesRepository implements ICategoriesRepository{
   private categories: Category[]
 
   constructor() {
     this.categories = []
   }
 
-  create({name, description}: ICreateCategoryDTIO): void {
+  create({name, description}: ICreateCategoryDTO): void {
     const category = new Category()
 
     Object.assign(category, {
